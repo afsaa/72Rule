a11b584726bdff9b7cfac505bab1990eb86d0d74
Andres Saa <andressaa.dev@gmail.com>
2019-02-14 20:51:32 -0500

    Base structure of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={{ fontFamily:'Roboto, sans-serif' }}>
+        <h1>Regla del 72</h1>
+        <h3>La regla del 72 es un cálculo que se realiza para saber cuanto tiempo nos toma doblar una inversión. Teniendo en cuenta: </h3>
+        <p>1. El rendimiento anual deseado. Ej:6% y <br />
+           2. La cantidad invertida. Ej: 100 USD<br />
+           Multiplicando estas dos variables obtendríamos aproximadamente el número de años necesarios para doblar esa inversión.
+        </p>
+        <h2>Como decía Albert Einstein: "El interés compuesto es la octava maravilla del mundo. El que lo entiende se beneficia... El que no... lo paga"</h2>
       </div>
     );
   }
